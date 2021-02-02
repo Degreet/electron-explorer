2406fbb5b9d444e123cf5868effd6128fbcedcc8
Degreet <degreetpro@gmail.com>
2021-02-02 18:32:41 +0200

    read dir module

---
+++ modules/readDir.js
@@ -0,0 +1,32 @@
+const fs = require('fs'),
+	fsp = fs.promises;
+
+async function readDir(path) {
+	const dir = await fsp.readdir(path);
+	const folders = [];
+	const files = [];
+
+	dir.forEach((item) => {
+		try {
+			const stat = fs.statSync(`${path}/${item}`);
+			const result = { name: item };
+
+			if (stat.isDirectory()) {
+				// folder
+				result.type = 'folder';
+				folders.push(result);
+			} else {
+				// file
+				result.type = 'file';
+				files.push(result);
+			}
+		} catch {}
+	});
+
+	return [...folders, ...files];
+}
+
+(async () => {
+	console.log(await readDir('C:/'));
+	while (true) {}
+})();
